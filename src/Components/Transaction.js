624496866513ea6c5c1b1bf1ceaad3f9129edb0b
Milad_Mrk <milad.rastgoou@gmail.com>
2022-04-10 19:53:11 +0430

    styles

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -1,18 +1,29 @@
 import React from "react";
 import { useDataTracker } from "./ProviderComponent";
+import styles from "./Transaction.module.scss";
 
-const Transaction = ({ className }) => {
+const Transaction = () => {
   const data = useDataTracker();
   return (
-    <div className={className}>
-      <div>filters</div>
+    <div className={styles.container}>
+      <div className={styles.filterBox}>filters</div>
       <div>
         {data.map((item) => {
           return (
-            <div key={item.id}>
-              <h2>TS</h2>
-              <h4>{item.detail}</h4>
-              <h4>{item.amount}</h4>
+            <div key={item.id} className={styles.detailBox}>
+              <div className={styles.boxOne}>
+                <h2>{item.detail.slice(0, 1).toUpperCase()}</h2>
+                <h4>{item.detail}</h4>
+              </div>
+              <h4
+                style={
+                  item.type === "expense"
+                    ? { color: "red" }
+                    : { color: "green" }
+                }
+              >
+                $ {item.amount}
+              </h4>
             </div>
           );
         })}
